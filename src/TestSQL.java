06f96ba6cc70580f62f4ddba3e81a0972e22d331
efeldel <man-9k@yandex.ru>
2022-06-15 22:58:53 +0300

    new method

--- src/TestSQL.java
+++ src/TestSQL.java
@@ -4,10 +4,8 @@ public class TestSQL {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         DB.conn();
-        //DB.create();
-        DB.insertCat("Лютик", "Домашняя", 5, 8.0);
-        DB.insertCat("Киса", "Дворовая", 2, 5.2);
-        DB.insertCat("Даша", "Гавана", 10, 12.5);
+        DB.create();
+        DB.addMoreCats(5000);
         DB.close();
     }
 }
